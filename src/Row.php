db5945fe8203af1546f42a1028f02e13adabd983
Salakhutdinov Salavat <salahutdinov.salavat@gmail.com>
2022-03-31 05:49:36 +0300

    add `getCells` method and some refactoring

--- src/Row.php
+++ src/Row.php
@@ -58,64 +58,93 @@ class Row implements ArrayAccess
     }
 
     /**
-     * @param  null  $nullValue
-     * @param  bool  $calculateFormulas
-     * @param  bool  $formatData
-     * @param  string|null  $endColumn
-     * @return Collection
+     * @param string $startColumn
+     * @param string|null $endColumn
+     * @return Cell[]
      */
-    public function toCollection($nullValue = null, $calculateFormulas = false, $formatData = true, ?string $endColumn = null): Collection
+    public function getCells(string $startColumn = 'A', ?string $endColumn = null): array
     {
-        return new Collection($this->toArray($nullValue, $calculateFormulas, $formatData, $endColumn));
-    }
-
-    /**
-     * @param  null  $nullValue
-     * @param  bool  $calculateFormulas
-     * @param  bool  $formatData
-     * @param  string|null  $endColumn
-     * @return array
-     */
-    public function toArray($nullValue = null, $calculateFormulas = false, $formatData = true, ?string $endColumn = null)
-    {
-        if (is_array($this->rowCache)) {
-            return $this->rowCache;
-        }
-
         $cells = [];
 
         $i = 0;
-        foreach ($this->row->getCellIterator('A', $endColumn) as $cell) {
-            $value = (new Cell($cell))->getValue($nullValue, $calculateFormulas, $formatData);
+        foreach ($this->row->getCellIterator($startColumn, $endColumn) as $cell) {
+            $cell = new Cell($cell);
 
             if (isset($this->headingRow[$i])) {
-                if (!$this->headerIsGrouped[$i]) {
-                    $cells[$this->headingRow[$i]] = $value;
+                if ($this->headerIsGrouped[$i]) {
+                    $cells[$this->headingRow[$i]][] = $cell;
                 } else {
-                    $cells[$this->headingRow[$i]][] = $value;
+                    $cells[$this->headingRow[$i]] = $cell;
                 }
             } else {
-                $cells[] = $value;
+                $cells[] = $cell;
             }
 
             $i++;
         }
 
-        if (isset($this->preparationCallback)) {
-            $cells = ($this->preparationCallback)($cells, $this->row->getRowIndex());
-        }
-
-        $this->rowCache = $cells;
-
         return $cells;
     }
 
     /**
-     * @param  bool  $calculateFormulas
+     * @param mixed $nullValue
+     * @param bool $calculateFormulas
+     * @param bool $formatData
+     * @param string|null $endColumn
+     * @return Collection
+     */
+    public function toCollection($nullValue = null, bool $calculateFormulas = false, bool $formatData = true, ?string $endColumn = null): Collection
+    {
+        return new Collection($this->toArray($nullValue, $calculateFormulas, $formatData, $endColumn));
+    }
+
+    /**
+     * @param mixed $nullValue
+     * @param bool $calculateFormulas
+     * @param bool $formatData
+     * @param  string|null  $endColumn
+     * @return array
+     */
+    public function toArray($nullValue = null, bool $calculateFormulas = false, bool $formatData = true, ?string $endColumn = null): ?array
+    {
+        if (is_array($this->rowCache)) {
+            return $this->rowCache;
+        }
+
+        $values = $this->getCellsValues($this->getCells('A', $endColumn), $nullValue, $calculateFormulas, $formatData);
+
+        if (isset($this->preparationCallback)) {
+            $values = ($this->preparationCallback)($values, $this->row->getRowIndex());
+        }
+
+        $this->rowCache = $values;
+
+        return $values;
+    }
+
+    /**
+     * @param array $cells
+     * @param mixed $nullValue
+     * @param bool $calculateFormulas
+     * @param bool $formatData
+     * @return array
+     */
+    protected function getCellsValues(array $cells, $nullValue = null, bool $calculateFormulas = false, bool $formatData = true): array
+    {
+        return array_map(function($cell) use ($nullValue, $calculateFormulas, $formatData) {
+            if (is_array($cell)) {
+                return $this->getCellsValues($cell, $nullValue, $calculateFormulas, $formatData);
+            }
+            return $cell->getValue($nullValue, $calculateFormulas, $formatData);
+        }, $cells);
+    }
+
+    /**
+     * @param bool $calculateFormulas
      * @param  string|null  $endColumn
      * @return bool
      */
-    public function isEmpty($calculateFormulas = false, ?string $endColumn = null): bool
+    public function isEmpty(bool $calculateFormulas = false, ?string $endColumn = null): bool
     {
         return count(array_filter($this->toArray(null, $calculateFormulas, false, $endColumn))) === 0;
     }
@@ -126,6 +155,16 @@ class Row implements ArrayAccess
     public function getIndex(): int
     {
         return $this->row->getRowIndex();
+    }
+
+    /**
+     * @param Closure|null $preparationCallback
+     *
+     * @internal
+     */
+    public function setPreparationCallback(Closure $preparationCallback = null): void
+    {
+        $this->preparationCallback = $preparationCallback;
     }
 
     #[\ReturnTypeWillChange]
@@ -150,15 +189,5 @@ class Row implements ArrayAccess
     public function offsetUnset($offset)
     {
         //
-    }
-
-    /**
-     * @param  \Closure  $preparationCallback
-     *
-     * @internal
-     */
-    public function setPreparationCallback(Closure $preparationCallback = null)
-    {
-        $this->preparationCallback = $preparationCallback;
     }
 }
